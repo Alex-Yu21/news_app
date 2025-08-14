6270b352809c1d65588dc3a357f2a0033b7902b2
Alex-Yu21 <yualex2112@gmail.com>
2025-08-14 20:25:18 +0200

    chore: register Dio, NewsApiRemote and repository in locator

--- lib/app/di/locator.dart
+++ lib/app/di/locator.dart
@@ -1,10 +1,23 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../data/datasources/remote/news_api_remote.dart';
+import '../../data/repositories/news_repository_impl.dart';
+import '../../domain/repositories/news_repository.dart';
 import '../network/dio_client.dart';
 
 final getIt = GetIt.instance;
 
 void setupLocator() {
-  getIt.registerLazySingleton<Dio>(DioClient.create);
+  if (!getIt.isRegistered<Dio>()) {
+    getIt.registerLazySingleton<Dio>(DioClient.create);
+  }
+  if (!getIt.isRegistered<NewsApiRemote>()) {
+    getIt.registerLazySingleton<NewsApiRemote>(() => NewsApiRemote(getIt()));
+  }
+  if (!getIt.isRegistered<NewsRepository>()) {
+    getIt.registerLazySingleton<NewsRepository>(
+      () => NewsRepositoryImpl(getIt()),
+    );
+  }
 }
