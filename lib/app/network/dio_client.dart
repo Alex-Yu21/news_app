b377b951310171c15e505d2b3615faba5ee72aec
Alex-Yu21 <yualex2112@gmail.com>
2025-08-14 13:17:08 +0200

    chore: add Dio via DioClient in get_it

---
+++ lib/app/network/dio_client.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+
+import 'api_key_interceptor.dart';
+
+class DioClient {
+  static Dio create() {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://newsapi.org/v2/',
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 15),
+        sendTimeout: const Duration(seconds: 10),
+        responseType: ResponseType.json,
+      ),
+    );
+
+    dio.interceptors.add(ApiKeyInterceptor());
+
+    assert(() {
+      dio.interceptors.add(
+        LogInterceptor(requestBody: false, responseBody: false),
+      );
+      return true;
+    }());
+
+    return dio;
+  }
+}
