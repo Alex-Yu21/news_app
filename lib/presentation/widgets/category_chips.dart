0084271d90939374a07422723bb193bb3f1fa68e
Alex-Yu21 <yualex2112@gmail.com>
2025-08-15 13:38:46 +0200

    style: CategoryChips

--- lib/presentation/widgets/category_chips.dart
+++ lib/presentation/widgets/category_chips.dart
@@ -1,38 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/app/theme/app_text.dart';
 import 'package:news_app/domain/enums/news_category.dart';
 
 class CategoryChips extends StatelessWidget {
+  const CategoryChips({super.key, required this.value, this.onSelected});
+
   final NewsCategory? value;
   final ValueChanged<NewsCategory?>? onSelected;
 
-  const CategoryChips({super.key, required this.value, this.onSelected});
+  static const _w = 114.0;
+  static const _h = 44.0;
+  static const _r = 22.0;
 
   @override
   Widget build(BuildContext context) {
+    final style = Theme.of(context).extension<AppText>()!;
+
     final cats = NewsCategory.values;
+
     return SizedBox(
-      height: 40,
+      height: _h,
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        itemCount: cats.length + 1,
-        separatorBuilder: (_, __) => const SizedBox(width: 8),
+        padding: EdgeInsets.zero,
+        clipBehavior: Clip.none,
+        itemCount: cats.length,
+        separatorBuilder: (_, __) => const SizedBox(width: 7),
         itemBuilder: (context, i) {
-          if (i == 0) {
-            final selected = value == null;
-            return ChoiceChip(
-              label: const Text('All'),
-              selected: selected,
-              onSelected: (_) => onSelected?.call(null),
-            );
-          }
-          final c = cats[i - 1];
+          final c = cats[i];
           final selected = value == c;
           final text = c.name[0].toUpperCase() + c.name.substring(1);
-          return ChoiceChip(
-            label: Text(text),
+
+          return RawChip(
+            showCheckmark: false,
+            labelPadding: EdgeInsets.zero,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            visualDensity: const VisualDensity(horizontal: -4, vertical: -4),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(_r),
+            ),
             selected: selected,
-            onSelected: (_) => onSelected?.call(c),
+            onSelected: (_) => onSelected?.call(selected ? null : c),
+            label: SizedBox(
+              width: _w,
+              height: _h,
+              child: Center(
+                child: Text(
+                  text,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  textHeightBehavior: const TextHeightBehavior(
+                    applyHeightToFirstAscent: false,
+                    applyHeightToLastDescent: false,
+                  ),
+                  strutStyle: const StrutStyle(
+                    forceStrutHeight: true,
+                    height: 0.1,
+                  ),
+                  style: style.caption.copyWith(color: Colors.white),
+                ),
+              ),
+            ),
           );
         },
       ),
