ba5318e7bcfe4741cc1e814a0818a9cb6bd2d71e
Alex-Yu21 <yualex2112@gmail.com>
2025-08-15 09:58:30 +0200

    feat: add Satoshi as default app font

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/app/di/locator.dart';
+import 'package:news_app/app/theme/app_theme.dart';
 import 'package:news_app/presentation/cubit/news_list_cubit.dart';
 
 import 'router.dart';
@@ -17,6 +18,7 @@ class App extends StatelessWidget {
       child: MaterialApp.router(
         title: 'News',
         debugShowCheckedModeBanner: false,
+        theme: buildAppTheme(),
         routerConfig: appRouter,
       ),
     );
